98963769072898a7746ae3c39defcac49b63a28b
andupont <adrien.dupont@cfa-afti.fr>
2014-04-04 19:07:37 +0200

    client lourd v0.1
    TODO :
    main window : liste collection à afficher
    new gem id : color picker a choisir
    connecter à la BDD
    écrire gemID JSON
    
    done : de l'IHM, des boutons des comboboxs des bindings

---
+++ Client Riche/JellyDiamonds/JDClientRicheCS/Commun.cs
@@ -0,0 +1,63 @@
+﻿namespace JDCommun 
+{
+    //enum Shapes
+    //{
+    //    Camer = "Camer",
+    //    Cushion = "Cushion",
+    //    Fancy = "Fancy",
+    //    Heart = "Heart",
+    //    Marquise = " Marquise",
+    //    Octagon = "Octagon",
+    //    Oval = "Oval",
+    //    Pear = "Pear",
+    //    Rectangle = "Rectangle",
+    //    Round = "Round",
+    //    Ruff = "Ruff",
+    //    Square = "Square",
+    //    Trillion = "Trillion",
+    //    Other = "Other"
+    //}
+ // List<String> shapes = new List<String> {};
+    // Ne peut pas etre déclaré dans un namespace
+        //List<String> shapes = new List<String>() ;
+        //this.shapes.Add("Camer");        shapes.Add("Oval");
+        //shapes.Add("Pear");         shapes.Add("Rectangle");
+        //shapes.Add("Round");        shapes.Add("Marquise");
+        //shapes.Add("Heart");        shapes.Add("Cushion");
+        //shapes.Add("Octagon");      shapes.Add("Square");       
+        //shapes.Add("Trillion");     shapes.Add("Fancy"); 
+        //shapes.Add("Ruff");         shapes.Sort();
+     //enum Cutting
+     //{
+     //    Asscher = "Asscher",
+     //    Baguette = "Baguette",
+     //    Briolette = "Briolette",
+     //    Cabashian = "Cabashian",
+     //    Checkerboard = " Checker-board",
+     //    Concave = "Concave",
+     //    Diamond = "Diamond",
+     //    Fancy = "Fancy",
+     //    Millenium = "Millenium",
+     //    Mixte = "Mixte",
+     //    Portugueese = "Portugueese",
+     //    Princess = "Princess",
+     //    Radiant = "Radiant",
+     //    Ruff = "Ruff",
+     //    Stepcut = "Step-cut",
+     //    Other = "Other"
+     //}
+     //enum Certificate
+     //{ 
+     // AIGS = "AIGS", //(Asian Institute Of Gemological Sciences)
+     // GIA = "GIA", //Gemmology institute
+     // Güblin = "Güblin", //(Swiss)
+     // HRD = "HRD", //(Anverse)
+     // Other = "Other"
+     //}
+     //enum Light
+     //{
+     //   Daylight = "Daylight",
+     //   Florescent = "Fluorescent",
+     //   Incandescent = "Incandescent"
+     //}
+}
---
+++ Client Riche/JellyDiamonds/JDClientRicheCS/Models/GemIDmodel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace JellyDiamondsCRicheC.Models
+{
+    public class GemID : ObservableCollection<string>
+    {
+
+
+        public List<string> Shapes = new List<string>()
+        {
+            "Camer","Cushion","Fancy","Heart","Marquise","Octagon",
+           "Oval","Pear","Rectangle","Round","Ruff","Square","Trillion","Other"
+        };
+        public List<string> Listshapes { get { return Shapes; } }
+
+        List<string> Cutting = new List<string>()
+        {
+         "Asscher","Baguette","Briolette","Cabashian"," Checker-board","Concave","Diamond",
+        "Fancy","Millenium","Mixte","Portugueese","Princess","Radiant","Ruff","Step-cut","Other"
+        };
+        public List<string> ListCutting { get { return Cutting; } }
+
+        List<string> Certificate = new List<string>() 
+     { 
+      "AIGS", //(Asian Institute Of Gemological Sciences)
+      "GIA", //Gemmology institute
+       "Güblin", //(Swiss)
+      "HRD", //(Anverse)
+      "Other"
+     };
+        public List<string> ListCertificate { get { return Certificate; } }
+
+        List<string> Light = new List<string>()
+       {
+           "Daylight",
+            "Fluorescent",
+           "Incandescent"
+       };
+        public List<string> ListLight { get { return Light;} }
+    }
+}
