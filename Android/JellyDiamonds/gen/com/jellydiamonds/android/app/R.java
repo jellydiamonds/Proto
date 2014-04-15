6c7639ad64cf693a6cc76d5f7fa548af95940cb1
JohnFoxx <moise.roussel@cfa-afti.fr>
2014-04-15 13:47:41 +0200

    Main screen.xml added

--- Android/JellyDiamonds/gen/com/jellydiamonds/android/app/R.java
+++ Android/JellyDiamonds/gen/com/jellydiamonds/android/app/R.java
@@ -30,23 +30,22 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int content_frame=0x7f090003;
-        public static final int distant_options_list=0x7f090008;
-        public static final int distant_title=0x7f090007;
-        public static final int drawer_Linearlayout=0x7f090004;
-        public static final int frag_gem_listview=0x7f090001;
-        public static final int local_options_list=0x7f090006;
-        public static final int local_title=0x7f090005;
-        public static final int root_drawer_layout=0x7f090002;
-        public static final int text1=0x7f090000;
+        public static final int action_settings=0x7f09000a;
+        public static final int content_frame=0x7f090004;
+        public static final int distant_options_list=0x7f090009;
+        public static final int distant_title=0x7f090008;
+        public static final int drawer_Linearlayout=0x7f090005;
+        public static final int local_options_list=0x7f090007;
+        public static final int local_title=0x7f090006;
+        public static final int root_drawer_layout=0x7f090003;
+        public static final int row_preview_gemid_picture=0x7f090000;
+        public static final int row_preview_gemid_specie=0x7f090001;
+        public static final int row_preview_gemid_summary=0x7f090002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int drawer_list_item=0x7f030001;
-        public static final int fragment_gem_item=0x7f030002;
-        public static final int fragment_gem_list=0x7f030003;
-        public static final int main_screen=0x7f030004;
+        public static final int drawer_list_item=0x7f030000;
+        public static final int gemid_row_preview_fragment=0x7f030001;
+        public static final int main_screen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -58,6 +57,9 @@ public final class R {
         public static final int distant_actions_title=0x7f050007;
         public static final int drawer_close=0x7f050003;
         public static final int drawer_open=0x7f050002;
+        /**  ROW PREVIEW STRING 
+         */
+        public static final int image_content_description=0x7f050008;
         /**  DRAWER STRINGS 
          */
         public static final int local_actions_title=0x7f050006;
